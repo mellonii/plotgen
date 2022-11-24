825c727484ab03a92d0aee1ab457781d6331746c
mellonii <91494089+mellonii@users.noreply.github.com>
2022-11-24 22:56:26 +0300

    new

--- prj.lab/UUU/UUU_test.cpp
+++ prj.lab/UUU/UUU_test.cpp
@@ -2,13 +2,24 @@
 #include <matplot/matplot.h>
 #include <set>
 
-void matplot_show() {
+void matplot_show(std::vector<double>& num1, std::vector<double>& num2, int i) {
     using namespace matplot;
-    auto x = transform(iota(1, 10), [](double x) { return pow(x, 2); });
-    plot(x);
-    title("My title");
+	plot(num1);
+	hold(on);
 
-    show();
+	plot(num2);
+	hold(off);
+
+    //show();
+	std::string name = "graf_" + std::to_string(i) + ".jpg";
+	//save(name);
+	save(name);
+}
+
+void matplot_show_one(std::vector<double>& num) {
+	using namespace matplot;
+	plot(num);
+	show();
 }
 
 #include <algorithm>
@@ -18,8 +29,7 @@ void matplot_show() {
 
 #include <chrono> 
 #include <fstream>
-
-void create_data_1(long int n, long int m){ // кол-во элементов, количество уникальности
+void create_data(long int n, long int m){ // кол-во элементов, количество уникальности
 	std::ofstream out;          // поток дл€ записи
 	out.open("prog_1.txt"); // окрываем файл дл€ записи
 	if (out.is_open()) // 20000
@@ -32,52 +42,38 @@ void create_data_1(long int n, long int m){ // кол-во элементов, количество уник
 }
 
 #include <string>
-#include <sstream>
-
 std::vector <int> invert(int n)
 {
 	std::string line;
 	std::vector <int> num(n);
 
-	std::ifstream in("prog_1.txt"); // окрываем файл дл€ чтени€
-	if (in.is_open())
-	{
-		while (getline(in, line))
-		{
-			//std::cout << line << std::endl;
-		}
+	std::ifstream fin;
+	fin.open("prog_1.txt");
+	for (int i = 0; i < n; i += 1) {
+		fin >> num[i];
 	}
-	in.close();     // закрываем файл
-
-	std::istringstream ss(line);
-	for (int i = 0; i < n; ++i) {
-		ss >> num[i];
-	}
+	fin.close();
 	return num;
 }
 
-void prog_1(int n, std::vector<int>& num) {
+#include <unordered_set>
+auto prog_1(int n, std::vector<int>& num) {
 	//прога находит столовку, которую дольше остальных не посещали
 
+	auto start_work = std::chrono::steady_clock::now();
 	std::vector<int> as(200001, -1);
 	for (int i = 0; i < n; i += 1) {
 		as[num[i]] = n - i - 1;
 	}
-
-	//std::cout << std::distance(as.begin(), std::max_element(as.begin(), as.end()));
+	std::distance(as.begin(), std::max_element(as.begin(), as.end()));
+	auto end_work = std::chrono::steady_clock::now();
+	return (end_work - start_work).count();
 }
+auto prog_2(int n, std::vector<int>& data) {
 
-#include <unordered_set>
-
-void prog_2(int n, std::vector<int>& num) {
-
-	std::vector<int> data(n, 0);
-	for (int i = 0; i < n; i += 1) {
-		std::cin >> data[i];
-		
-	}
+	auto start_work = std::chrono::steady_clock::now();
 	std::unordered_set<int> unique;
-	unique.reserve(200000);
+	unique.reserve(200000+1);
 	int idx_unique = n;
 	for (int i = n - 1; 0 <= i; i -= 1) {
 		if (unique.find(data[i]) != unique.end()) {
@@ -85,57 +81,49 @@ void prog_2(int n, std::vector<int>& num) {
 			unique.insert(idx_unique);
 		}
 	}
+	auto end_work = std::chrono::steady_clock::now();
+	return (end_work - start_work).count();
 	//std::cout << idx_unique;
 }
 
-
-#include <unordered_set>
-/*
-void prog_2() {
-	int n = 0;
-	std::cin >> n;
-	std::vector<int> data(n, 0);
-	for (int i = 0; i < n; i += 1) {
-		std::cin >> data[i];
-
-	}
-	std::unordered_set<int> unique;
-	unique.reserve(200000);
-	int idx_unique = n;
-	for (int i = n - 1; 0 <= i; i -= 1) {
-		if (!unique.contains(data[i])) {
-			idx_unique = data[i];
-			unique.insert(idx_unique);
-
-		}
-
-	}
-	std::cout << idx_unique;
-
-}*/
-
-#include <algorithm>
-
 int main() {
+	srand(time(NULL));
+	std::ofstream out;
+	out.open("otchet.md");
+	out << "The program generates for each m (from 1 to 200000 with a step of m *= 2) n integers with values from 1 to 200000 with a step of n *= 1.318, where m is the number of unique values. "
+		<< "Then it passes these values through the given two programs and calculates their operating time using chrono and builds graphs with axes n and t/n. \n\n"
+		<< "Link to [github](https://github.com/mellonii/plotgen)\n"
+		<< "Prepared by: Ishkuvatova Milena\n\n";
+
 	std::vector <int> num;
-	std::vector <std::vector <int>> second(10);
-	for (int m = 1; m <= 10; m++) {
-		for (int n = 1000; n <= 20000; n = n + 1000) {
+
+	for (int m = 1; m <= 200000; m *= 2) {
+		std::vector <double> y_1;
+		std::vector <double> y_2;
+		for (double i = 1; i <= 200000; i *= 1.318) {
+			int n = ceil(i);
+			create_data(n, m);
 			auto start_work = std::chrono::steady_clock::now();
-			create_data_1(n, m);
 			num = invert(n);
-			prog_1(n, num);
 			auto end_work = std::chrono::steady_clock::now();
-			second[m - 1].push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end_work - start_work).count());
-			num.clear();
+			//std::chrono::duration_cast<std::chrono::milliseconds>(end_work - start_work).count() - врем€ обращени€ к данным
+			//std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end_work - start_work).count() << "ms ";
+			//std::cout << num.size() << " " <<  prog_2(n, num) << "\n";
+			y_1.push_back(prog_1(n, num) / (double)(n * 100000));
+			y_2.push_back(prog_2(n, num) / (double)(n * 100000));
 		}
+
+		matplot_show(y_1, y_2, m);
+		std::string name = "graf_" + std::to_string(m) + ".jpg";
+		out << "M = " << m << "\n![["<< name <<"]] \n";
 	}
-	for (int m = 0; m < 10; m++) {
-		for (int i = 0; i < second.size(); i++) {
-			std::cout << second[m][i] << " ";
-		}
-		std::cout << "\n";
-	}
+
+	//matplot_show(y_1, y_2);
+
+	//matplot_show_one(y_1);
+	//matplot_show_one(y_2);
+	
+	out.close();
 
 	return 0;
 }
@@ -148,22 +136,3 @@ int main() {
 //n и t/n оси
 // k - коэф уникальных значений к n - процент уникальных значений от всех значений
 //автоматическое тестирование проги при разных значени€х
-
-/*
-1 2 6 3 1 2 3 1 // уникальные значени€ - какие могут встречатьс€ 
-
-1 -> 8735
-2 -> 4357
-3 -> 34951
-6 
-
-n
-unordered_map<int,int>{ не поределен хеш дл€ вектора и тд (а мапу норм)
-    m[1] = 7
-    m[2] = 5
-    m[3] = 6
-    m[6] = 2
-}
-
-
-*/
